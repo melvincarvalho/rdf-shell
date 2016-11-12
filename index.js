f8638bbeec810e47c575a9a3e6ed9dbcf96fbb97
Melvin Carvalho <melvincarvalho@gmail.com>
2016-11-12 14:28:57 +0100

    Add mv function closes #11

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ module.exports = {
   cat: require('./lib/cat'),
   cp: require('./lib/cp'),
   ls: require('./lib/ls'),
+  mv: require('./lib/mv'),
   patch: require('./lib/patch'),
   util: require('./lib/util')
 }
