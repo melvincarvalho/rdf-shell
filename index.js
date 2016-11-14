5d68837f808292017a371cadbdde51d71d3d407a
Melvin Carvalho <melvincarvalho@gmail.com>
2016-11-14 15:29:10 +0100

    add rm to index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
   cp: require('./lib/cp'),
   ls: require('./lib/ls'),
   mv: require('./lib/mv'),
+  rm: require('./lib/rm'),
   patch: require('./lib/patch'),
   util: require('./lib/util')
 }
